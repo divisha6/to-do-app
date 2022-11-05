fe9c195fcc0c851121b039cc75cd17c02b149667
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2022-11-05 13:49:59 +0530

    edited TodoForm and TodoList components

---
+++ src/components/TodoForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random() *10000),
+        text: input
+    });
+
+    setInput(''); 
+}
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  }
+
+  return (
+    <div>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a task to do"
+          value={input}
+          name="text"
+          className="todo-input"
+          onChange={handleChange}
+        />
+
+        <button className="todo-button">
+            Add task
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
